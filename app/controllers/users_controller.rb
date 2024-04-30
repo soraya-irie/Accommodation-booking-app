1f96b711fb0e210f5fd2057d8ba344a38b20cd65
soraya-ire <rujiangkongshi@gmail.com>
2024-04-30 14:52:33 +0900

    アカウントとプロフィールの編集画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,14 @@ class UsersController < ApplicationController
   end
 
   def profile
+    @user = current_user
   end
 
   def profile_edit
     @user = current_user
   end
+end
+
+def user_params
+  params.requrei(:user).permit(:name, :email, :password_digest, :self_introduction, :icon-image, :created_at, :updated_at)
 end
